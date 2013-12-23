023f75725f5d71e7a44ebeaaf0cba56bca387915
Victoria Popic <viq@stanford.edu>
2013-12-23 01:22:46 -0800

    search k dist

--- align.h
+++ align.h
@@ -2,7 +2,8 @@
 #define ALIGN_H_
 #include "io.h"
 #include "cluster.h"
+#include "index.h"
 
-void align_reads(ref_t* ref, reads_t* reads);
+void align_reads(ref_t* ref, reads_t* reads, index_params_t* params);
 
 #endif /*ALIGN_H_*/
