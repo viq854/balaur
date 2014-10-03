37355e5082f7d9fea09ec93d3c42a17ab6c547bb
Victoria Popic <viq@stanford.edu>
2014-10-03 11:48:24 -0700

    exposing dist_best

--- src-cpp/main.cc
+++ src-cpp/main.cc
@@ -101,7 +101,7 @@ int main(int argc, char *argv[]) {
 	params.set_default_index_params();
 
 	int c;
-	while ((c = getopt(argc-1, argv+1, "i:o:w:k:h:L:H:T:b:p:l:g:St:I:m:s:")) >= 0) {
+	while ((c = getopt(argc-1, argv+1, "i:o:w:k:h:L:H:T:b:p:l:g:St:I:m:s:d:")) >= 0) {
 		switch (c) {
 			case 'i': params.in_index_fname = std::string(optarg); break;
 			case 'o': params.out_index_fname = std::string(optarg); break;
@@ -119,6 +119,7 @@ int main(int argc, char *argv[]) {
 			case 'I': params.hit_collection_interval = atoi(optarg); break;
 			case 'm': params.min_n_hits = atoi(optarg); break;
 			case 's': params.bucket_size = atoi(optarg); break;
+			case 'd': params.dist_best_hit = atoi(optarg); break;
 			default: return 0;
 		}
 	}
