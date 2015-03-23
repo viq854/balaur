7393f6f11e6ad07ae5f05f8def01674f60c92fbf
Victoria Popic <viq@dn0a2301c0.sunet>
2015-03-22 18:18:45 -0700

    - sampling, + loc on ref

--- src-cpp/types.h
+++ src-cpp/types.h
@@ -39,8 +39,8 @@ typedef std::vector<uint8> VectorU8;
 typedef std::vector<bool> VectorBool;
 typedef std::vector<hash_t> VectorHash;
 typedef std::vector<minhash_t> VectorMinHash;
-typedef std::vector<seq_t, tbb::scalable_allocator<seq_t>> VectorSeqPos;
-//typedef std::vector<loc_t, tbb::scalable_allocator<loc_t>> VectorSeqPos;
+//typedef std::vector<seq_t, tbb::scalable_allocator<seq_t>> VectorSeqPos;
+typedef std::vector<loc_t, tbb::scalable_allocator<loc_t>> VectorSeqPos;
 typedef std::map<uint32, seq_t> MapKmerCounts;
 
 
