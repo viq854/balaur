8075d924d8e8d2e7d28b324a271c7cd879f5af81
Victoria Popic <viq@stanford.edu>
2015-03-23 21:57:17 -0700

    out of core index

--- src-cpp/io.h
+++ src-cpp/io.h
@@ -47,7 +47,7 @@ void store_perm(const char* permFname, const VectorU32& perm);
 void load_perm(const char* permFname, VectorU32& perm);
 void store_hash_pads(const char* permFname, const VectorHash& perm);
 void load_hash_pads(const char* permFname, VectorHash& perm);
-void store_ref_idx_per_thread(const int tid, const bool first_entry, const char* refFname, const ref_t& ref, const index_params_t* params);
+void store_ref_idx_per_thread(const int tid, const bool first_entry, const char* refFname, ref_t& ref, const index_params_t* params);
 void load_ref_idx_per_thread(const int tid, const char* refFname, ref_t& ref, index_params_t* params);
 
 // compression
