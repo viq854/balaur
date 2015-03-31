4d1d7a12a035e0a7106efef10c37a5e549585c9a
Victoria Popic <viq@dn0a23020e.sunet>
2015-03-30 21:10:47 -0700

    rc ref match fix

--- src-cpp/align.cc
+++ src-cpp/align.cc
@@ -813,10 +813,16 @@ int eval_read_hit(ref_t& ref, read_t* r, const index_params_t* params) {
 		   uint32_t match_pos = match.pos;
 		   if(match.rc) {
 			   match_pos = ref.len - match.pos;
-		   }
-		   if(pos_l >= match_pos - match.len - 1300 && pos_l <= match_pos + 1300) {
-			   r->acc = 1;
-			   break;
+			   if(pos_l >= match_pos - 1300 && pos_l <= match_pos + match.len + 1300) {
+				   r->acc = 1;
+				   break;
+			   }
+		   } else {
+			   match_pos = match.pos;
+			   if(pos_l >= match_pos - match.len - 1300 && pos_l <= match_pos + 1300) {
+				   r->acc = 1;
+				   break;
+			   }
 		   }
 	   }
 	   if(r->acc == 1) {
