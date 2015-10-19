103d82f4ebe3935a6cd67c94a00b51937a33f603
Victoria Popic <viq@stanford.edu>
2015-10-19 14:11:46 -0700

    precompute packed 32bp kmers

--- src-cpp/align.h
+++ src-cpp/align.h
@@ -7,6 +7,6 @@
 void align_reads_lsh(ref_t& ref, reads_t& reads, const index_params_t* params);
 void align_reads_minhash(ref_t& ref, reads_t& reads, const index_params_t* params);
 void align_reads_sampling(ref_t& ref, reads_t& reads, const index_params_t* params);
-void balaur_main(const ref_t& ref, reads_t& reads, const index_params_t* params);
+void balaur_main(ref_t& ref, reads_t& reads, const index_params_t* params);
 
 #endif /*ALIGN_H_*/
